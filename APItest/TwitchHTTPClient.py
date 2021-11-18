20fa6274cf31247cbac4f42538e8fda212a908fd
CrazyQie <728977862@qq.com>
2021-11-18 10:11:08 -0500

    api test

---
+++ APItest/TwitchHTTPClient.py
@@ -0,0 +1,52 @@
+#%%
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Columbia EECS E6893 Big Data Analytics
+
+"""
+This module is used to pull data from twitch API and preprocess the data
+
+"""
+
+import twitch
+
+# Credentials
+client_id = '918pxgmrcct2dbuwu7tih5na23ogxq'
+oauth_token = 'if6rhr2bfebpxqlie36i66bazekgz0'
+
+client = twitch.TwitchHelix(client_id=client_id, oauth_token=oauth_token)
+streams = client.get_streams()  # APICursor containing Stream objects
+streams_top20 = streams._queue  # List of Stream objects
+
+
+#%%
+def stream_preprocess(streams):
+    """
+
+    :param streams: list of Stream objects
+    :return:
+    {'data': [{'id': ***, 'user_id': ***, 'user_login': ***, 'user_name': ***, ..., 'tag_ids': ***, 'is_mature': ***},
+              {'id': ***, 'user_id': ***, 'user_login': ***, 'user_name': ***, ..., 'tag_ids': ***, 'is_mature': ***},
+              ...
+              ]
+    }
+    """
+
+    fields = ['id', 'user_id', 'user_login', 'user_name', 'game_id', 'game_name', 'type', 'title', 'viewer_count',
+              'started_at', 'language', 'thumbnail_url', 'tag_ids', 'is_mature']
+    data = {'data': []}
+    for stream in streams:
+        print(stream['id'], stream['viewer_count'])
+        d = {}
+        for field in fields:
+            if field == 'started_at':
+                d[field] = stream[field].strftime('%Y-%m-%d %H:%M:%S')
+            else:
+                d[field] = stream[field]
+        data['data'].append(d)
+
+    return data
+
+
+data = stream_preprocess(streams)
+
