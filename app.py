90b6335d4e38fb5dc89c3b638ae42756e0695f07
CrazyQie <728977862@qq.com>
2021-11-19 01:57:49 -0500

    update topKgame function

--- app.py
+++ app.py
@@ -16,27 +16,6 @@ CORS(app, support_credentials = True)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
-@app.route('/topK/<num>', methods=['GET'])
-def topK(num):  # put application's code here
-    if request.method == 'GET':
-        num = int(num)
-        data = TwitchHTTPClient.getTopK(num)
-        print(data)
-        # data = {'data':
-        #             [
-        #                 {'id': '40230484139', 'user_id': '83232866', 'user_login': 'ibai', 'user_name': 'ibai',
-        #                  'game_id': '518203', 'game_name': 'Sports', 'type': 'live', 'title': 'PADEL DE LAS ESTRELLAS 2 | EL TORNEO DE PADEL DEL AÑO ',
-        #                  'viewer_count': 112452, 'started_at': '2021-11-18 18:13:47', 'language': 'es',
-        #                  'thumbnail_url': 'https://static-cdn.jtvnw.net/previews-ttv/live_user_ibai-{width}x{height}.jpg',
-        #                  'tag_ids': ['d4bb9c58-2141-4881-bcdc-3fe0505457d1'], 'is_mature': False}
-        #             ]
-        # }
-        result = {}
-        roomList = data['data']
-        gameList = GetGraph.gameAndViewerGraph(roomList)
-        result['gameAndViewerGraph'] = gameList
-        return json.dumps(result)
-
 @app.route('/topKGames/<num>', methods=['GET'])
 def topKGames(num):  # put application's code here
     if request.method == 'GET':
