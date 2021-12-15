554a8363955c7a3a26bfeefa1c4dc37b7886b38e
CrazyQie <728977862@qq.com>
2021-12-15 03:40:35 -0500

    Create barChartForTopKGames.js

---
+++ templates/barChartForTopKGames.js
@@ -0,0 +1,56 @@
+var domain = "http://127.0.0.1:5000";
+var BarChartForTopKGames = echarts.init(document.getElementById('barChartForTopKGames'), null, {
+    height: 500,
+    width: 700
+});
+
+function drawBarChartForTopKGames(dataSet) {
+    var option = {
+        title: {
+            left: 'left',
+            text: 'TOP 10 Popular Live Games'
+        },
+        dataset: [
+            {
+                dimensions: ['game', 'viewer'],
+                source: dataSet
+            },
+            {
+                transform: {
+                    type: 'sort',
+                    config: {dimension: 'viewer', order: 'desc'}
+                }
+            }
+        ],
+        xAxis: {
+            type: 'category',
+            axisLabel: {
+                interval: 0,
+                rotate: 15,
+                textStyle: {fontSize: 11}
+            }
+        },
+        yAxis: {
+            textStyle: {fontSize: 12}
+        },
+        series: {
+            type: 'bar',
+            encode: {x: 'name', y: 'score'},
+            datasetIndex: 1
+        }
+    };
+    BarChartForTopKGames.setOption(option);
+}
+
+var barDataSetForTopKGames;
+$.ajax({
+    url: domain + "/topKGames/" + 10,
+    type: 'GET',
+    cache: false,
+    processData: false,
+    contentType: 'application/json',
+    success: function (r) {
+        barDataSetForTopKTags = JSON.parse(r);
+        drawBarChartForTopKGames(barDataSetForTopKTags);
+    }
+})
