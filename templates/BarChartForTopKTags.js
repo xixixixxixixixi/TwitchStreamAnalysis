ac459a087652158ebb6a700cc3a0b78011227ae7
CrazyQie <728977862@qq.com>
2021-12-15 03:46:58 -0500

    Update BarChartForTopKTags.js

--- templates/BarChartForTopKTags.js
+++ templates/BarChartForTopKTags.js
@@ -7,3 +7,56 @@ window.onload = function () {
     drawBarChartForTopKTags([]);
 }
 
+function drawBarChartForTopKTags(dataSet) {
+    var option = {
+        title: {
+            left: 'left',
+            text: 'Most Popular Tags'
+        },
+        dataset: [
+            {
+                dimensions: ['tag', 'count'],
+                source: dataSet
+            },
+            {
+                transform: {
+                    type: 'sort',
+                    config: {dimension: 'count', order: 'desc'}
+                }
+            }
+        ],
+        xAxis: {
+            type: 'category',
+            axisLabel: {
+                interval: 0,
+                rotate: 15,
+                textStyle: {fontSize: 12}
+            }
+        },
+        yAxis: {
+            textStyle: {fontSize: 13}
+        },
+        series: {
+            type: 'bar',
+            encode: {x: 'name', y: 'score'},
+            datasetIndex: 1,
+            itemStyle: {color: '#3BA272'}
+        }
+    };
+    BarChartForTopKTags.setOption(option);
+}
+
+// ajax for top K Tags
+var barDataSetForTopKTags;
+$.ajax({
+    url: domain + "/topKTags/" + 10,
+    type: 'GET',
+    cache: false,
+    processData: false,
+    contentType: 'application/json',
+    success: function (r) {
+        barDataSetForTopKGames = JSON.parse(r);
+        drawBarChartForTopKTags(barDataSetForTopKGames);
+    }
+})
+
