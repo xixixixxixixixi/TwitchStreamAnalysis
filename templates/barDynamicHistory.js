fdf1e5323bc2c85166eb76352c9bd66ef1dbced3
CrazyQie <728977862@qq.com>
2021-12-15 05:02:43 -0500

    up

--- templates/barDynamicHistory.js
+++ templates/barDynamicHistory.js
@@ -7,6 +7,67 @@ window.onload = function () {
     drawBarDynamicHistory([]);
 }
 
+function drawBarDynamicHistory(dataSet) {
+    if (dataSet == null || dataSet.length === 0) {
+        return;
+    }
+    const dataToShow = [];
+    // const data2 = dataSet.data;
+    let viewerCount = dataSet["viewerCount"];
+    let label = dataSet["gameLabel"];
+    for (let i = 0; i < viewerCount.length; ++i) {
+        dataToShow.push(100);
+    }
+    option = {
+        xAxis: {
+            max: 'dataMax'
+        },
+        yAxis: {
+            type: 'category',
+            data: label,
+            inverse: true,
+            animationDuration: 300,
+            animationDurationUpdate: 300,
+            max: 9 // only the largest 3 bars will be displayed
+        },
+        series: [
+            {
+                realtimeSort: true,
+                name: 'X',
+                type: 'bar',
+                data: dataToShow,
+                label: {
+                    show: true,
+                    position: 'right',
+                    valueAnimation: true
+                }
+            }
+        ],
+        legend: {
+            show: true
+        },
+        animationDuration: 0,
+        animationDurationUpdate: 3000,
+        animationEasing: 'linear',
+        animationEasingUpdate: 'linear'
+    };
+
+    function run() {
+        for (var i = 0; i < viewerCount.length; ++i) {
+            for (var j = 0; j < 5; ++j) {
+                dataToShow[i] = viewerCount[i][j];
+                barDynamicHistory.setOption(option);
+            }
+
+        }
+    }
+
+    setInterval(function () {
+        run();
+    }, 3000);
+}
+
+
 var dataSetForHistoryDynamicChart;
 $.ajax({
     url: domain + "/getDynamicPopularGamesBarChart",
